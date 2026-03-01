285b82785ee05f231b522521e389e6fac836a8de
AruihaYoru <AruihaYoru@gmail.com>
2026-03-01 11:04:23 +0900

    定期習性

--- docs/Verse/main.js
+++ docs/Verse/main.js
@@ -1,11 +1,22 @@
-document.addEventListener('DOMContentLoaded', function() {
-  const images = document.querySelectorAll('.image img');
-  images.forEach(img => {
-    const link = document.createElement('a');
-    link.href = img.src;
-    link.className = 'luminous';
-    link.appendChild(img.cloneNode());
-    img.parentNode.replaceChild(link, img);
-    new Luminous(link);
-  });
+document.addEventListener('DOMContentLoaded', async function() {
+    await Cozy.parseAll();
+
+    const images = document.querySelectorAll('.image img');
+    
+    images.forEach(img => {
+        const link = document.createElement('a');
+        
+        link.href = img.src; 
+        link.className = 'luminous';
+        
+        const caption = img.closest('.image')?.getAttribute('data-caption');
+        
+        const newImg = img.cloneNode();
+        link.appendChild(newImg);
+        img.parentNode.replaceChild(link, img);
+
+        new Luminous(link, {
+            caption: caption
+        });
+    });
 });
